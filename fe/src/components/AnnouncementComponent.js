6895ecebc7720175155b7282cff9a6060fd2c41c
Vu Minh Phuong <vu.minhphuong@petronas.com>
2024-08-22 12:25:50 +0800

    open pdf file by new link, clean code

--- fe/src/components/AnnouncementComponent.js
+++ fe/src/components/AnnouncementComponent.js
@@ -1,18 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import { pdfjs } from "react-pdf";
 import { Link } from 'react-router-dom';
 import '../assets/css/page.css';
 import * as constants from "../jscode/constants";
 import * as utils from "../jscode/utilities";
 
-pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.6.172/pdf.worker.min.js';
-
 export default function AnnouncementComponent() {  
     const [announcement, setAnnouncement] = useState({});
     const [files, setFiles] = useState([]);
-    const [showModal, setShowModal] = useState(false);
     const [attTitle, setAttTitle] = useState(constants.string_empty);
 
     const getAnnouncement = async () => {        
@@ -47,49 +42,6 @@ export default function AnnouncementComponent() {
                 let blobData = new Blob([pdfData], { type: 'application/pdf' });
                 const fileURL = URL.createObjectURL(blobData);
                 window.open(fileURL, "_blank");
-
-                /*
-                var len = pdfdata.length;
-                var bytes = new Uint8Array(len);
-                for (var i = 0; i < len; i++){
-                    bytes[i] = pdfdata.charCodeAt(i);                    
-                }
-                renderPdf = bytes.buffer;
-                const pdfFile = new Blob([renderPdf], { type: 'application/pdf' });
-                const fileURL = URL.createObjectURL(pdfFile);
-                window.open(fileURL, "_blank");
-                const url = window.URL.createObjectURL(pdfFile);
-                */
-
-        // Create a temporary <a> element to trigger the download
-        /*
-        const tempLink = document.createElement("a");
-        tempLink.href = url;
-        tempLink.setAttribute(
-          "download",
-          `bill.pdf`
-        ); // Set the desired filename for the downloaded file
-
-        // Append the <a> element to the body and click it to trigger the download
-        document.body.appendChild(tempLink);
-        tempLink.click();
-
-        // Clean up the temporary elements and URL
-        document.body.removeChild(tempLink);
-        window.URL.revokeObjectURL(url);
-        */
-                /*               
-                var len = pdfdata.length;
-                var bytes = new Uint8Array(len);
-                for (var i = 0; i < len; i++){
-                    bytes[i] = pdfdata.charCodeAt(i);                    
-                }
-                renderPdf = bytes.buffer;
-                console.log(renderPdf);
-                setFileContent(renderPdf);   
-                console.log(fileContent);  
-                */           
-                //setShowModal(true);
             })
             .catch(error => {
                 console.log("Error: ", error);
@@ -120,26 +72,6 @@ export default function AnnouncementComponent() {
           </div>
         </div>
       </div>
-
-
-      <Modal
-            show={showModal}
-            fullscreen={true}
-            onHide={() => setShowModal(false)}
-            dialogClassName="modal-90w main hero section  dark-background"
-            aria-labelledby="example-custom-modal-styling-title"
-        >
-            <Modal.Header closeButton>
-            <Modal.Title id="example-custom-modal-styling-title">
-                Noi dung thong bao
-            </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="container col-xxl-12 col-xl-12 col-lg-112 col-md-12 col-sm-12 col-12">
-                
-                </div>
-            </Modal.Body>
-        </Modal>
     </main>
     )
   }
